41d008924c41c582eb46a3742d87b9bbaf32aa34
jinchao <chjinhust@gmail.com>
2014-11-20 10:35:52 +0800

    added install_ceph.sh

--- ceph-scripts/destroy_cluster.sh
+++ ceph-scripts/destroy_cluster.sh
@@ -4,6 +4,7 @@ if [ "$#" -lt 2 ]
 then
   echo "usage: sudo $0 mon_name osd1_name:data_dir:journal_dev [osd2_name...]"
   echo "example: sudo $0 monitor OSD1:/mnt/osd1:/dev/sdb1 OSD2:/mnt/osd2:/dev/sdb1"
+  echo "example: sudo $0 node185 node102:/mnt/osd1:/dev/sdb1 node109:/mnt/osd2:/dev/sdb1"
   exit 0
 fi
 
