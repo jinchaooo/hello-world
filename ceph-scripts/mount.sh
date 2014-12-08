cce64e2d45f2091982c6a2a38b6cafe5294680a0
jinchao <chjinhust@gmail.com>
2014-12-08 11:21:00 +0800

    m mount.sh

--- ceph-scripts/mount.sh
+++ ceph-scripts/mount.sh
@@ -7,12 +7,20 @@ then
   exit 0
 fi
 
+osdev="/dev/sdd1"
+jnrdev="/dev/sde3"
+cachedev="/dev/sde2"
+
+mkdir -p /mnt/os
+mkdir -p /mnt/jnr
+mkdir -p /mnt/cache
+
 if [ $# -ne 1 ]
 then 
   echo "usage: $0 btrfs/ext4/xfs"
-  echo "mkfs on /dev/sdd1 (hdd) => /mnt/os,"
-  echo "        /dev/sde3 (ssd) => /mnt/jnr,"
-  echo "        /dev/sde2 (ssd) => /mnt/cache,"
+  echo "mkfs on $osdev (hdd) => /mnt/os,"
+  echo "        $jnrdev (ssd) => /mnt/jnr,"
+  echo "        $cachedev (ssd) => /mnt/cache,"
   exit 0
 fi
 
@@ -32,25 +40,25 @@ if mountpoint -q /mnt/os
 then
   echo "/mnt/os already mounted"
 else
-  echo "mkfs on /dev/sdd1 mount to /mnt/os"
-  sudo $MKFS /dev/sdd1
-  sudo mount -t "$1" /dev/sdd1 /mnt/os
+  echo "mkfs on $osdev mount to /mnt/os"
+  sudo $MKFS $osdev
+  sudo mount -t "$1" $osdev /mnt/os
 fi
 
 if mountpoint -q /mnt/jnr
 then
   echo "/mnt/jnr already mounted"
 else
-  echo "mkfs on /dev/sde3 mount to /mnt/jnr"
-  sudo $MKFS /dev/sde3
-  sudo mount -t "$1" /dev/sde3 /mnt/jnr
+  echo "mkfs on $jnrdev mount to /mnt/jnr"
+  sudo $MKFS $jnrdev
+  sudo mount -t "$1" $jnrdev /mnt/jnr
 fi
 
 if mountpoint -q /mnt/cache
 then
   echo "/mnt/cache already mounted"
 else
-  echo "mkfs on /dev/sde2 mount to /mnt/cache"
-  sudo $MKFS /dev/sde2
-  sudo mount -t "$1" /dev/sde2 /mnt/cache
+  echo "mkfs on $cachedev mount to /mnt/cache"
+  sudo $MKFS $cachedev
+  sudo mount -t "$1" $cachedev /mnt/cache
 fi
